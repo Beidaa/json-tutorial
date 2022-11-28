db8732e76ef020a86f47a2b69cb1d636a701dbd2
beidaa <ahahahskr@gmail.com>
2022-11-28 16:51:28 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -13,9 +13,9 @@ enum {
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
-
+//解析json
 int lept_parse(lept_value* v, const char* json);
-
+//访问结果，获取类型
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
